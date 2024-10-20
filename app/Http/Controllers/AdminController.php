841216e5852a184a96942cecaa4a36b60ed1aa86
Khadija <sweetytanha63@gmail.com>
2024-10-20 21:31:24 +0600

    admin login add

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class AdminController extends Controller
 {
     
     public function AdminDashboard(){
         return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request){
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }
+
+    public function AdminLogin(Request $request){
+        return view('admin.admin_login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function(){
     Route::get('admin/dashboard', [AdminController::class,'AdminDashboard'])->name('admin.dashboard');
+
+    Route::get('admin/logout',[AdminController::class,'AdminLogout'])->name('admin.logout');
 });
 
 Route::middleware(['auth', 'role:agent'])->group(function(){
@@ -31,7 +33,7 @@ Route::middleware(['auth', 'role:agent'])->group(function(){
 });
 
 
-
+Route::get('admin/login',[AdminController::class,'AdminLogin'])->name('admin.login');
 
 
 // Route::get('user/dashboard', [UserController::class,'UserDashboard'])->name('user.dashboard');
